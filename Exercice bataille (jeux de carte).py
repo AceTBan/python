c049a91da05b0b876fd88bf765b7986ce3ed09bb
AceTBan <94051289+AceTBan@users.noreply.github.com>
2021-12-22 15:31:00 +0100

    exo bataille

--- Exercice bataille (jeux de carte).py
+++ Exercice bataille (jeux de carte).py
@@ -1,7 +1,7 @@
 
-
+# importe la fonction aleatoire
 import random
-
+# creation de trois listes (deck qui contient les cartes du jeu - j1 les cartes du joueur n1 & j2 du joueur n2)
 deck = []
 j1 = []
 j2 = []
@@ -9,41 +9,41 @@ j2 = []
 
 
 
-
+# fonction "distribution" qui va permettre de melanger les cartes et les distribuer aleatoirement au joueur
 def distribution(j2,j1,deck):
-    for i in range(2,14):
+    for i in range(2,15):
         deck.append(i)
         deck.append(i)
         deck.append(i)
         deck.append(i)
-
+    # fois 4 parce que (coeur, pique, trefle, coeur)
     random.shuffle(deck)
     for j in range(0,52,2):
         carte1 = deck[j]
         j1.append(carte1)
         carte2 = deck[j+1]
         j2.append(carte2)
-        return j1,j2
+    return j1,j2
 
-
+# fonction "tour" permet le bon fonctionnement du jeu
 def tour(j1,j2):
     carte1=j1[0]
     carte2=j2[0]
-    if carte1<carte2:
-        j2.pop(0)
-        j1.append(carte1)
-        j1.pop(0)
-        j1.append(carte2)
-        affichage(j1,j2,carte1,carte2)
-        print("j1 gagne")
-
-    elif carte1>carte2:
+    if carte1<carte2:                   # celui qui "pop" la carte l'"append" a l'autre
         j1.pop(0)
         j2.append(carte1)
         j2.pop(0)
         j2.append(carte2)
         affichage(j1,j2,carte1,carte2)
-        print("j2 gagne")
+        print("joey gagne le tour de jeu")
+
+    elif carte1>carte2:
+        j2.pop(0)
+        j1.append(carte2)
+        j1.pop(0)
+        j1.append(carte1)
+        affichage(j1,j2,carte1,carte2)
+        print("kaiba gagne la manche ")
 
     elif carte1==carte2:
         print("exeko")
@@ -51,8 +51,8 @@ def tour(j1,j2):
         exeko_j2 = []
         exeko_j1.append(carte1)
         exeko_j2.append(carte2)
-        j1.pop(0)
         j2.pop(0)
+        j1.pop(0)
         affichage(j1,j2,carte1,carte2)
         while carte1==carte2:
             exeko_j1.append(j1[0])
@@ -66,25 +66,25 @@ def tour(j1,j2):
             j1.pop(0)
             j2.pop(0)
 
-            if carte1>carte2:
-                j1+exeko_j1
-                j1+exeko_j2
-                affichage(j1,j2,carte1,carte2)
-                print("j1 gagne")
-
-            elif carte1<carte2:
+            if carte1<carte2:
                 j2 + exeko_j1
                 j2 + exeko_j2
                 affichage(j1,j2,carte1,carte2)
-                print("j1 gagne")
+                print("joey gagne la bataille")
+
+            elif carte1>carte2:
+                j1+exeko_j1
+                j1+exeko_j2
+                affichage(j1,j2,carte1,carte2)
+                print("kaiba gagne la bataille")
 
 
 
 
-
+# fonction "affichage" permet d'afficher les figures (as, roi, dame, valet)
 def affichage(j1,j2,carte1,carte2):
     if carte1==14:
-        carte1 ="as"
+        carte1="as"
     if carte2==14:
         carte2="as"
     if carte1==13:
@@ -99,20 +99,18 @@ def affichage(j1,j2,carte1,carte2):
         carte1="valet"
     if carte2==11:
         carte2="valet"
-    print("j1:",carte1,"j2",carte2)
+    print("kaiba draw an",carte1,"&","joey draw an",carte2)
 
 
 
 
-
-
+distribution(j1,j2,deck)
+# boucle de jeu (si un joueur n'a plus de carte il perd la partie sinon continue)
 while j1 or j2:
     if not j1:
-        print ("j1 perd")
+        print ("kaiba perd alors qu'il a 3 dragon blanc aux yeux bleu O_O' ")
         break
     if not j2:
-        print ("j2 perd")
+        print ("joey perd mais il a un dragon noir aux yeux rouge hyper styler alors ca va ^^")
     else:
         tour(j1,j2)
-        print("commencer?")
-        continuer=input()
